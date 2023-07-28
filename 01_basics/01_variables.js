a2c68fdcd838986ac1e47b87439e564d09ddff4e
Vipul Kumar <vipulnitsri2000@gmail.com>
2023-07-28 08:37:58 +0000

    Variables file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144453
+let accountEmail = "hellovipul1998@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+// const accountId: any
+// accountId =2
+
+accountEmail = "hc@hc.com"
+accountPassword = "12345678"
+accountCity = "benglore"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
