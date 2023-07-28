339f53bd66536448cd07ce06cc0905691fc72e7a
Vipul Kumar <vipulnitsri2000@gmail.com>
2023-07-28 09:11:58 +0000

    Data types file is added

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3=3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("vipul")
+
+let name = "Vipul"//string data types
+let age = 18 // number data type
+let isLoggedIn = false // boolean data type
+
+//number = 2 to power 53
+//bigint 
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined = 
+//symbol = unique
+
+//object 
+
+console.log(typeof "vipul")
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
