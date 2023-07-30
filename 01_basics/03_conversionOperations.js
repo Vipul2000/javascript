65cb65fa4d5bd747d5d0a5312bf8807e14926b91
Vipul Kumar <vipulnitsri2000@gmail.com>
2023-07-30 18:27:53 +0000

    ConversionOperations file added

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,12 @@
+/* 
+Coercion:- whenever we do operationn based on the input we can actually convert the inpurt for the operation
+- we can convert type of input.
+ 
+TypeCasting or type conversion.
+This conversion can be manually done by us is called explicit conversion
+and the conversion is done on some certain rules automatically converts the types is called implicit conversion.
+
+Abstract operations:- These are some seta of algorithms that is present in the ecmascripts does they are not available for useage on ecmascripts.
+i.e- we developers cannot use these operations directly.
+
+*/
